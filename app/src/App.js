642792c142455762f8a2672527f18748e6b00d4d
Alexandr Timchenko <atimchenko92@gmail.com>
2018-05-01 16:04:42 +0200

    Refactored the provider initialization

--- app/src/App.js
+++ app/src/App.js
@@ -3,12 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 import { default as Web3} from 'web3';
 import { default as contract } from 'truffle-contract';
+import election_artifacts from './contracts/Election.json';
 
-// Import our contract artifacts and turn them into usable abstractions.
-import election_artifacts from './contracts/Election.json'
 var Election = contract(election_artifacts);
-
-const provider = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+var provider;
 
 var accounts;
 var account;
@@ -35,12 +33,12 @@ class App extends Component {
   }
 
   componentWillMount(){
-    var self = this;
-
+    this.initializeProvider();
     // Bootstrap the MetaCoin abstraction for Use.
     Election.setProvider(provider.currentProvider);
 
     // Get the initial account balance so it can be displayed.
+    var self = this;
     provider.eth.getAccounts(function(err, accs) {
       if (err != null) {
         alert("There was an error fetching your accounts.");
@@ -56,6 +54,23 @@ class App extends Component {
       account = accounts[0];
       self.myFct();
     });
+  }
+
+  initializeProvider(){
+    if (typeof provider !== 'undefined') {
+      console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
+      // Use Mist/MetaMask's provider
+      provider = new Web3(Web3.currentProvider);
+    } else {
+      console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+      // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+      provider = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    }
+  }
+
+  makeVote(){
+  //TODO
+
   }
 
   render() {
