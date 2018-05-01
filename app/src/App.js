8d7dba4d14c53a7b452bf2056711b538f4f4ea5a
Alexandr Timchenko <atimchenko92@gmail.com>
2018-05-01 21:24:08 +0200

    Fixed error connect to asynchronous call with web3 initialization.

--- app/src/App.js
+++ app/src/App.js
@@ -15,7 +15,6 @@ class App extends Component {
       loading: true,
       voting: false,
     }
-    window.addEventListener('load', function() {
     // Checking if Web3 has been injected by the browser (Mist/MetaMask)
     if (typeof web3 !== 'undefined') {
       console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
@@ -26,9 +25,6 @@ class App extends Component {
       // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
       window.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
     }
-
-    });
-
   }
 /*
   myFct(){
@@ -47,7 +43,6 @@ class App extends Component {
 
   componentWillMount(){
     var self = this;
-    // Get the initial account balance so it can be displayed.
     window.web3.eth.getCoinbase(function(err, account) {
       if (err != null) {
         alert("There was an error fetching your account.");
