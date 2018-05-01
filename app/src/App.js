486945550c37885ae1af106f59c42f5d35f92542
AzizShaumarov <aziz.shaumarov@gmail.com>
2018-05-01 18:42:47 +0200

    Metamask compobility

--- app/src/App.js
+++ app/src/App.js
@@ -5,67 +5,65 @@ import { default as Web3} from 'web3';
 import { default as contract } from 'truffle-contract';
 import election_artifacts from './contracts/Election.json';
 
-var Election = contract(election_artifacts);
-var provider;
-
-var accounts;
-var account;
-
 class App extends Component {
   constructor(args){
     super(args);
     this.state = {
+      account: '0x0',
+      candidates: [],
+      hasVoted: false,
+      loading: true,
+      voting: false,
     }
-  }
-
-  myFct(){
-    var self = this;
-    var elect;
-
-    Election.deployed().then(function(instance) {
-      elect = instance;
-      return elect.candidatesCount.call();
-    }).then(function(value) {
-      console.log(value.valueOf());
-    }).catch(function(e) {
-      console.log(e);
-    });
-  }
-
-  componentWillMount(){
-    this.initializeProvider();
-    // Bootstrap the MetaCoin abstraction for Use.
-    Election.setProvider(provider.currentProvider);
-
-    // Get the initial account balance so it can be displayed.
-    var self = this;
-    provider.eth.getAccounts(function(err, accs) {
-      if (err != null) {
-        alert("There was an error fetching your accounts.");
-        return;
-      }
-
-      if (accs.length == 0) {
-        alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
-        return;
-      }
-
-      accounts = accs;
-      account = accounts[0];
-      self.myFct();
-    });
-  }
-
-  initializeProvider(){
-    if (typeof provider !== 'undefined') {
+    window.addEventListener('load', function() {
+    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+    if (typeof web3 !== 'undefined') {
       console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
       // Use Mist/MetaMask's provider
-      provider = new Web3(Web3.currentProvider);
+      window.web3 = new Web3(window.web3.currentProvider);
     } else {
-      console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+      console.warn("No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
       // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-      provider = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+      window.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
     }
+
+    });
+
+  }
+/*
+  myFct(){
+
+    this.Election.deployed().then(function(instance) {
+      this.electInstance = instance;
+      return this.elect.candidatesCount.call();
+    }).then(function(value) {
+      console.log(value.valueOf());
+      console.log(e);
+    }).catch(function(e) {
+    });
+  }*/
+
+
+
+  componentWillMount(){
+    var self = this;
+    // Get the initial account balance so it can be displayed.
+    window.web3.eth.getCoinbase(function(err, account) {
+      if (err != null) {
+        alert("There was an error fetching your account.");
+        console.log(err);
+        return;
+      }
+
+      if (account == null) {
+        alert("Couldn't load account! Make sure your Ethereum client is configured correctly.");
+        return;
+      }
+
+      self.setState({account: account});
+      console.log(self.state.account);
+
+    });
   }
 
   makeVote(){
